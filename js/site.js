b5ebd3e1d481c21fae6ec75f1bdc80ac75ec3aa7
Abdoulie Njai <64009576+Phalconn@users.noreply.github.com>
2021-07-24 05:17:53 -0700

    program features added

--- js/site.js
+++ js/site.js
@@ -1,4 +1,45 @@
-//Call Hello World
-function helloWorld(){
-    alert("hello World");
+//get the string from the page
+//controller fucntion
+function getValue(){
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+
+}
+
+//Reverse the string
+//logic function
+function reverseString(userString){
+
+    let revString = [];
+
+    //let name = Abdoulie
+    //name[0] = "A";
+    //name[4] = "u";
+    //last position in an array is name.length - 1
+
+    //reverse a string using a for loop
+    for (let index = userString.length - 1 ; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+    return revString;
+
+}
+
+//display the message with reverse string to the user
+//view function
+function displayString(revstring){
+
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revstring}`;
+
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+
 }
